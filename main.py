a25b872a3bf9ab724e24b28218d70825038ac2cf
Peu-PC <phvb20@gmail.com>
2023-11-08 19:47:38 -0300

    refeito o front com as propriedades do streamlit / css tambem refeito agora mais enxuto

--- main.py
+++ main.py
@@ -9,76 +9,66 @@ import base64
 # Configuração da página
 st.set_page_config(page_title='Rent Up', layout='wide')
 
-
-def get_image_as_base64(filepath):
-    with open(filepath, "rb") as img_file:
-        return base64.b64encode(img_file.read()).decode('utf-8')
-
+# Injeção de CSS personalizado
 def local_css(file_name):
-    with open(file_name, 'r', encoding='utf-8') as f:
+    with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def remote_css(url):
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
-
-def icon(icon_name):
-    return f'<i class="material-icons">{icon_name}</i>'
-
 local_css("style.css")
-remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-encoded_image = get_image_as_base64('img/logo.png')
+# Header
+header_col1, header_col2, header_col3 = st.columns([1, 8, 2])
 
-# Custom header com a imagem codificada em base64
-st.markdown(f"""
-    <div class="header">
-        <img src="data:image/png;base64,{encoded_image}" class="logo">
-        <div class="search-box">
-            <span class="material-icons">search</span>
-            <input type="text" class="search-input" placeholder="Procure por algo">
-        </div>
-        <span class="material-icons icon">account_circle</span>
-        <span class="material-icons icon">shopping_cart</span>
-    </div>
-    <div class="navigation">
-        <button class="nav-btn active">HOME</button>
-        <button class="nav-btn">CATEGORIAS</button>
-        <button class="nav-btn">DESTAQUES</button>
-        <button class="nav-btn">SUPORTE</button>
-        <button class="nav-btn">MINHA CONTA</button>
-    </div>
-""", unsafe_allow_html=True)
+with header_col1:
+    logo = Image.open('img/logo.png')
+    st.image(logo, width=100)
 
-# Barra de navegação secundária
-st.markdown("""
-<div class="secondary-nav">
-        <button class="sec-nav-btn active">Mais buscados</button>
-        <button class="sec-nav-btn">Novidades</button>
-        <button class="sec-nav-btn">Kits Arduinos</button>
-        <button class="sec-nav-btn">Fórum GARAgino</button>
-    </div>
-""", unsafe_allow_html=True)
+with header_col2:
+    st.text_input("", placeholder="Procure por algo")
 
-# Footer
-st.markdown("""
-    <div class="footer">
-        <div class="footer-section">
-            <h5>Quem somos</h5>
-            <a href="#">Conheça a RentUp</a>
-            <a href="#">Nossos produtos</a>
-            <a href="#">Lorem ipsum</a>
-        </div>
-        <div class="footer-section">
-            <h5>Comunidade</h5>
-            <a href="#">Garagem</a>
-            <a href="#">GaRAgino</a>
-            <a href="#">Lorem ipsum</a>
-        </div>
-        <div class="footer-section">
-            <h5>Ajuda</h5>
-            <a href="#">Falar com suporte</a>
-            <a href="#">Lorem ipsum</a>
-            <a href="#">Lorem ipsum</a>
-        </div>
-    </div>
-""", unsafe_allow_html=True)
+with header_col3:
+    st.button("Account")
+    st.button("Shopping Cart")
+
+# SideBar
+# Lista de botões para a navegação principal
+nav_buttons = ['HOME', 'CATEGORIAS', 'DESTAQUES', 'SUPORTE', 'MINHA CONTA']
+
+# Loop para adicionar cada botão na sidebar
+for button in nav_buttons:
+    st.sidebar.button(button)
+
+# Navegação secundária
+# Insere linha divisória
+st.markdown("""---""")  
+sec_nav_buttons = ['Mais buscados', 'Novidades', 'Kits Arduinos', 'Fórum GARAgino']
+sec_nav_cols = st.columns(len(sec_nav_buttons)) # Seleciona a quantidade de colunas a partir da quantidade de itens no array "sec_nav_buttons"
+for i, button in enumerate(sec_nav_buttons):
+    with sec_nav_cols[i]:
+        st.button(button)
+
+#Footer
+# Insere outra linha divisória
+st.markdown("---")  
+
+# Cria 3 colunas com cabeçalhos e botões
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.subheader("Quem somos")
+    st.button("Conheça a RentUp")
+    st.button("Nossos produtos")
+
+with col2:
+    st.subheader("Comunidade")
+    st.button("Garagem")
+    st.button("GARAgino")
+
+with col3:
+    st.subheader("Ajuda")
+    st.button("Falar com suporte")
+
+# Insere uma linha divisória
+st.markdown("""---""") 
+footer_col1, footer_col2, footer_col3 = st.columns(3)
+with footer_col1:
+    st.write("Rent Up © 2023")
